e829b908c12283528b3ac4c7280f77be62749094
kylingo <studiotang.cn@gmail.com>
2017-06-14 11:54:11 +0800

    Add generate html

--- primer/mongo_db.py
+++ primer/mongo_db.py
@@ -1,7 +1,12 @@
 # coding=utf-8
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
 import pymongo
 
-print ('pymongo test')
+from thirdparty import makeHTML
+
+print('pymongo test')
 client = pymongo.MongoClient('localhost', 27017)
 db = client.scrapy
 connection = db.jobs
@@ -16,13 +21,29 @@ connection = db.jobs
 #                   )
 
 # query
-item_one = connection.find_one()
+# item_one = connection.find_one()
+# print('item_one', item_one)
+
 count = connection.find().count()
 item_all = connection.find()
-print ('item_one', item_one)
-print ('count', count)
+print('count', count)
 for item in item_all:
-    print ('item', item)
+    print('item', item)
 
 # remove
-connection.remove()
+# connection.remove()
+
+
+# make html table
+## 数据需要重新处理
+item_one = connection.find_one()
+
+rows = connection.find()
+pageHead = makeHTML.head('Hello World')
+pageBody = makeHTML.body('Emergency Table System')
+html_table = makeHTML.table(rows).make()
+# print ('html_table:', html_table)
+
+# pageBodhy.addPiece(html_table)
+# page = makeHTML.page([pageHead, pageBody])
+# page.make()
